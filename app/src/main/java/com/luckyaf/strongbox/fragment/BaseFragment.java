c4b9627c985379dabc07b5210e19c02cd2fd0a3a
luckyAF <944235771@qq.com>
2016-03-02 08:46:57 +0800

    basefragment

--- app/src/main/java/com/luckyaf/strongbox/fragment/BaseFragment.java
+++ app/src/main/java/com/luckyaf/strongbox/fragment/BaseFragment.java
@@ -1,17 +1,86 @@
 package com.luckyaf.strongbox.fragment;
 
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+
+import com.luckyaf.strongbox.R;
+
 
 /**
  * 类描述
- * @FileName BaseFragment.java
+ *
  * @author Created by Xcf on 2016/2/24.
+ * @FileName BaseFragment.java
  */
-public class BaseFragment extends Fragment implements View.OnClickListener{
+public abstract class BaseFragment extends Fragment implements View.OnClickListener {
+
+    protected String fragmentName = "BaseFragment";
+
 
     @Override
-    public void onClick(View v) {
-
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        return inflater.inflate(R.layout.basefragment_layout, container, false);
     }
+
+    public abstract BaseFragment newInstance(Bundle args);
+
+
+    public abstract void initWidget();
+
+    public abstract void initData();
+
+    public abstract void initListener();
+
+
+
+    @Override
+    public void onDestroy() {
+        // TODO Auto-generated method stub
+        super.onDestroy();
+    }
+
+
+
+    @Override
+    public void onPause() {
+        // TODO Auto-generated method stub
+        Log.i(fragmentName, "onPause");
+        super.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        // TODO Auto-generated method stub
+        Log.i(fragmentName, "onResume");
+        super.onResume();
+    }
+
+    @Override
+    public void onStart() {
+        // TODO Auto-generated method stub
+        Log.i(fragmentName, "onStart");
+        super.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        // TODO Auto-generated method stub
+        Log.i(fragmentName, "onStop");
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroyView() {
+        // TODO Auto-generated method stub
+        Log.i(fragmentName, "move to other view");
+        super.onDestroyView();
+    }
+
+
 }
